ed74bfa0eaa927d157f6ef39bee3eed616726cf1
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-03-27 12:31:23 -0300

    Estrutura inicial do projeto

---
+++ ordem-management/src/main/java/br/com/miguel/ordem_management/OrdemManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.miguel.ordem_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdemManagementApplication.class, args);
+	}
+
+}
---
+++ ordem-management/src/test/java/br/com/miguel/ordem_management/OrdemManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.miguel.ordem_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
