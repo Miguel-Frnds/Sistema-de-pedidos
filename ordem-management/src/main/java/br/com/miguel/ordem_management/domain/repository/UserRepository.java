2b37fc086822288fedd38f0560fab15cf2cf9d5e
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-03-27 20:32:46 -0300

    Cria repository de User

---
+++ ordem-management/src/main/java/br/com/miguel/ordem_management/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.miguel.ordem_management.domain.repository;
+
+import br.com.miguel.ordem_management.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmailAndIdNot(String email, Long id);
+    boolean existsByUsernameAndIdNot(String username, Long id);
+}
