163dd8b3c0ec17dce18280c8eb37f2199556ffc2
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-03-28 14:40:43 -0300

    Adciona ProductMapper para conversão entre entidade e DTO

---
+++ ordem-management/src/main/java/br/com/miguel/ordem_management/api/mapper/ProductMapper.java
@@ -0,0 +1,26 @@
+package br.com.miguel.ordem_management.api.mapper;
+
+import br.com.miguel.ordem_management.api.dto.product.ProductCreateRequestDTO;
+import br.com.miguel.ordem_management.api.dto.product.ProductResponseDTO;
+import br.com.miguel.ordem_management.domain.entity.Product;
+
+public class ProductMapper {
+    public static Product toEntity(ProductCreateRequestDTO dto){
+        Product product = new Product();
+        product.setName(dto.name());
+        product.setDescription(dto.description());
+        product.setPrice(dto.price());
+        product.setQuantity(dto.quantity());
+        return product;
+    }
+
+    public static ProductResponseDTO toResponseDTO(Product product){
+        return new ProductResponseDTO(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                product.getQuantity()
+        );
+    }
+}
