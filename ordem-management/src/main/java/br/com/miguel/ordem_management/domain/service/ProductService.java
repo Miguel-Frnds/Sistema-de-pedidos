71ec44670633b23a763d3b2714e4d28ed43513f0
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-03-28 14:43:53 -0300

    Cria service de Product com regras de negócio iniciais

---
+++ ordem-management/src/main/java/br/com/miguel/ordem_management/domain/service/ProductService.java
@@ -0,0 +1,81 @@
+package br.com.miguel.ordem_management.domain.service;
+
+import br.com.miguel.ordem_management.api.dto.product.ProductCreateRequestDTO;
+import br.com.miguel.ordem_management.api.dto.product.ProductResponseDTO;
+import br.com.miguel.ordem_management.api.dto.product.ProductStockRequestDTO;
+import br.com.miguel.ordem_management.api.dto.product.ProductUpdateRequestDTO;
+import br.com.miguel.ordem_management.api.mapper.ProductMapper;
+import br.com.miguel.ordem_management.domain.entity.Product;
+import br.com.miguel.ordem_management.domain.exception.InsufficientStockException;
+import br.com.miguel.ordem_management.domain.exception.ProductNotFoundException;
+import br.com.miguel.ordem_management.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<ProductResponseDTO> findAll(){
+        return productRepository.findAll().stream()
+                .map(ProductMapper::toResponseDTO)
+                .toList();
+    }
+
+    public ProductResponseDTO findById(Long id){
+        return ProductMapper.toResponseDTO(getById(id));
+    }
+
+    public ProductResponseDTO save(ProductCreateRequestDTO product){
+        Product createProduct = ProductMapper.toEntity(product);
+        return ProductMapper.toResponseDTO(productRepository.save(createProduct));
+    }
+
+    public ProductResponseDTO update(Long id, ProductUpdateRequestDTO request){
+        Product foundProduct = getById(id);
+
+        if(request.name() != null && !request.name().isBlank()){
+            foundProduct.setName(request.name());
+        }
+        if(request.description() != null && !request.description().isBlank()){
+            foundProduct.setDescription(request.description());
+        }
+
+        if(request.price() != null) {
+            foundProduct.setPrice(request.price());
+        }
+
+        return ProductMapper.toResponseDTO(productRepository.save(foundProduct));
+    }
+
+    public ProductResponseDTO addStock(Long id, ProductStockRequestDTO request){
+        Product foundProduct = getById(id);
+        foundProduct.setQuantity(foundProduct.getQuantity() + request.quantity());
+        return ProductMapper.toResponseDTO(productRepository.save(foundProduct));
+    }
+
+    public ProductResponseDTO removeStock(Long id, ProductStockRequestDTO request){
+        Product foundProduct = getById(id);
+
+        if(foundProduct.getQuantity() < request.quantity()){
+            throw new InsufficientStockException(foundProduct.getName());
+        }
+
+        foundProduct.setQuantity(foundProduct.getQuantity() - request.quantity());
+        return ProductMapper.toResponseDTO(productRepository.save(foundProduct));
+    }
+
+    public void delete(Long id){
+        Product foundProduct = getById(id);
+        productRepository.delete(foundProduct);
+    }
+
+    private Product getById(Long id){
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(id));
+    }
+}
