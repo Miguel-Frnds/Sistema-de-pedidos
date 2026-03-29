410034181976796bca01e5423af4b66f7e17ced0
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-03-29 15:59:59 -0300

    Refatora DTO para receber apenas name e description

--- ordem-management/src/main/java/br/com/miguel/ordem_management/api/dto/product/ProductUpdateRequestDTO.java
+++ ordem-management/src/main/java/br/com/miguel/ordem_management/api/dto/product/ProductUpdateRequestDTO.java
@@ -1,11 +1,7 @@
 package br.com.miguel.ordem_management.api.dto.product;
 
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
-import java.math.BigDecimal;
-
 public record ProductUpdateRequestDTO(@Size(min = 2, max = 100) String name,
-                                      String description,
-                                      @Positive BigDecimal price) {
+                                      String description) {
 }
