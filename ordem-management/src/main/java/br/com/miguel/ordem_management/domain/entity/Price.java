50dfcd3bd476fde7dc101e40222f5beb06ab8bf1
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-03-29 15:53:28 -0300

    Adiciona Annotations a entidade Price

--- ordem-management/src/main/java/br/com/miguel/ordem_management/domain/entity/Price.java
+++ ordem-management/src/main/java/br/com/miguel/ordem_management/domain/entity/Price.java
@@ -1,10 +1,36 @@
 package br.com.miguel.ordem_management.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "prices")
+@EqualsAndHashCode(of = "id")
 public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
     private Product product;
-    private Double value;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal value;
+
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @PrePersist
+    private void prePersist(){
+        this.createdAt = LocalDateTime.now();
+    }
 }
