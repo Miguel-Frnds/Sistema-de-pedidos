1b3306a502193a29b1dd8c99604fa6839742bbc1
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-03-29 15:59:09 -0300

    Adiciona tratamento de exception centralizado para SamePriceException e PriceNotFoundException

--- ordem-management/src/main/java/br/com/miguel/ordem_management/api/handler/RestExceptionHandler.java
+++ ordem-management/src/main/java/br/com/miguel/ordem_management/api/handler/RestExceptionHandler.java
@@ -24,7 +24,13 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<RestErrorMessage> ProductNotFoundHandler(ProductNotFoundException exception){
+    public ResponseEntity<RestErrorMessage> productNotFoundHandler(ProductNotFoundException exception){
+        RestErrorMessage error = new RestErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage(), LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(PriceNotFoundException.class)
+    public ResponseEntity<RestErrorMessage> priceNotFoundHandler(PriceNotFoundException exception){
         RestErrorMessage error = new RestErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage(), LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
@@ -47,8 +53,14 @@ public class RestExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
+    @ExceptionHandler(SamePriceException.class)
+    public ResponseEntity<RestErrorMessage> SamePriceHandler(SamePriceException exception){
+        RestErrorMessage error = new RestErrorMessage(HttpStatus.BAD_REQUEST, exception.getMessage(), LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<RestErrorMessage> MethodArgumentNotValidHandler(MethodArgumentNotValidException exception){
+    public ResponseEntity<RestErrorMessage> methodArgumentNotValidHandler(MethodArgumentNotValidException exception){
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
         Map<String, String> errors = new HashMap<>();
         for (FieldError fieldError : fieldErrors) {
